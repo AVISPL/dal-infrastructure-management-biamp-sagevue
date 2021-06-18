98513b8e4425b25d33080fa4c3c534654836fdb0
Maksym Rossiitsev <maksym.rossiitsev@avispl.com>
2021-06-18 17:47:57 +0300

    1.0.5 release preparation

--- src/test/java/com/avispl/symphony/dal/communicator/biamp/sagevue/SageVueCommunicatorTest.java
+++ src/test/java/com/avispl/symphony/dal/communicator/biamp/sagevue/SageVueCommunicatorTest.java
@@ -54,7 +54,7 @@ public class SageVueCommunicatorTest {
         Assert.assertFalse(devices.isEmpty());
         Assert.assertEquals("03275657", devices.get(0).getSerialNumber());
         Assert.assertEquals("172.31.254.129", devices.get(0).getProperties().get("ipAddress"));
-        Assert.assertEquals(19, ((AdvancedControllableProperty.DropDown)devices.get(0).getControllableProperties().get(1).getType()).getOptions().length);
+        Assert.assertEquals(19, ((AdvancedControllableProperty.DropDown)devices.gmvn et(0).getControllableProperties().get(1).getType()).getOptions().length);
         Assert.assertEquals("123|ERR:Unable to get information\n456|ERR2:Unable to fetch information\n789|FWER:Firmware error\n", devices.get(0).getProperties().get("deviceFaults"));
         Assert.assertEquals("Rebooting", devices.get(0).getProperties().get("occupiedStatus"));
     }
